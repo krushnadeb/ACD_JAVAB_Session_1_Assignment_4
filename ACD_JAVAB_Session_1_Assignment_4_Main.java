75ae77e0a7470c8efa3504bb157d4572ced876d0
krushnadeb <krushna_deb@rediffmail.com>
2016-04-16 20:16:41 +0530

    Added files via upload

---
+++ ACD_JAVAB_Session_1_Assignment_4_Main.java
@@ -0,0 +1,20 @@
+//Assume that the integer variable x is 5 and the integer variable y is 10. Give the values of the following
+//expressions:
+//a. x + y * 2
+//b. x - y + 2
+//c. (x+y) * 2
+//d. y % x
+public class ACD_JAVAB_Session_1_Assignment_4_Main {
+	public static void main(String[] args) {	
+		int x=5,y=10;
+					
+		System.out.println("x+y*2 = 25 ;" );
+		System.out.println(x+y*2);
+		System.out.println("x-y+2 = -3 ;" );
+		System.out.println(x-y+2 );	
+		System.out.println("(x+y) * 2 = 30 ;" );
+		System.out.println((x+y) * 2 );	
+		System.out.println("y % x = 0 ;" );
+		System.out.println(y % x);	
+		}
+}
